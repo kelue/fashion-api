aac416a3901aac77068c5663091e6ad1c1995a4e
kelue <austincmoon5@gmail.com>
2022-10-24 11:54:13 +0100

    setup get route to view all items

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ const express = require("express")
 const cors = require("cors")
 const db = require("./config/db.config.js")
 
+//routes
+const itemsRouter = require("./routers/items")
+
 const app = express()
 //midlewares
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
 const port = process.env.PORT || 8080
 
-app.get("/", (req, res)=>{
-    res.end("Hello Fashion")
-})
+app.use("/api/v1/fashion", itemsRouter)
+
+
+//redirect requests to root to version 1 of the api
+app.get("/", (req, res) => {
+    res.redirect("/api/v1/fashion");
+});
 
 app.listen(port, ()=>{
     console.log(`app running on port ${port}`)
