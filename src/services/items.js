df7458982295b40ffd3d343cf82b80d7f1156fa5
kelue <austincmoon5@gmail.com>
2022-10-26 14:41:01 +0100

    refactor SQL queries to a single file

--- src/services/items.js
+++ src/services/items.js
@@ -1,33 +1,30 @@
 const db = require("../config/db.config")
+const { selectAllItemsDB, selectItemDB, addItemDB, updateItemDB, deleteItemDB } = require("../database/queries") //db queries
 
 //get all the items in the database
 const getAll = async ({limit, offset}) => {
     //query database and retrieve items in descending order with limited items and offset
-    const items = await db.any('SELECT * FROM items ORDER BY id DESC LIMIT $(limit) OFFSET $(offset)', {limit, offset})
+    const items = await db.any(selectAllItemsDB, {limit, offset})
     return items
 }
 
 //get a  single item with the id
 const getItem = async (id) => {
-    const getResult = await db.oneOrNone('SELECT id FROM items WHERE id = $(id)', {id})
+    const getResult = await db.oneOrNone(selectItemDB, {id})
     return getResult
 }
 
 // add an item to the database
 const addItem = async ({ name, brand, color }) => {
 
-    const item = await db.one('INSERT INTO items(name, brand, color) VALUES($(name), $(brand), $(color)) RETURNING id', {name, brand, color})
+    const item = await db.one(addItemDB, {name, brand, color})
     return item
 }
 
 //update an existing item to the database
 const updateItem = async ({id, name, brand, color }) => {
 
-    const result = db.one(`
-    UPDATE items SET name = $(name), brand = $(brand), color = $(color) 
-    WHERE id = $(id)
-    RETURNING id, name AS new_name, brand AS new_brand, color AS new_color
-    `, {name, brand, color, id})
+    const result = db.one(updateItemDB, {name, brand, color, id})
     
     return result
 }
@@ -35,7 +32,7 @@ const updateItem = async ({id, name, brand, color }) => {
 //delete an existing item in the database
 const deleteItem = async ({ id }) => {
     
-    const result = db.one('DELETE FROM items WHERE id = $(id) RETURNING id', {id})
+    const result = db.one(deleteItemDB, {id})
     return result
 }
 
